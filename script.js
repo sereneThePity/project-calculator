da1772ffb6893f5cb8db0558c213494dc72096e9
Serene The Pity <weekndsonn@gmail.com>
2023-07-17 21:40:26 +0200

    Display digits when clicked

--- script.js
+++ script.js
@@ -13,9 +13,24 @@ function divide(num1,num2){
 }
 
 function operatre(num1,num2,operator){
+    num1 = +num1;
+    num2 = +num2;
     if (operator==='+') return add(num1,num2);
     if (operator==='-') return subtract(num1,num2);
     if (operator==='*') return multiply(num1,num2);
     if (operator==='/') return divide(num1,num2);
+}
+
+let display = document.querySelector('.display')
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('operator');
+
+digits.forEach(elem=>elem.addEventListener('click',show))
+
+function show(){
+    if (display.textContent!=0){
+        return display.textContent += this.textContent;
+    }
+    display.textContent = this.textContent;
 }
 
