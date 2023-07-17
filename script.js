11e42f7239359c114e982da0dd2f1cb624ea2662
Serene The Pity <weekndsonn@gmail.com>
2023-07-17 22:01:07 +0200

    Basic calculator => operand => operator => operand => equal => answer

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function divide(num1,num2){
     return num1 / num2;
 }
 
-function operatre(num1,num2,operator){
+function operate(num1,num2,operator){
     num1 = +num1;
     num2 = +num2;
     if (operator==='+') return add(num1,num2);
@@ -21,11 +21,14 @@ function operatre(num1,num2,operator){
     if (operator==='/') return divide(num1,num2);
 }
 
-let display = document.querySelector('.display')
-const digits = document.querySelectorAll('.digit');
-const operators = document.querySelectorAll('operator');
+let firstNumber,secondNumber,operator;
 
-digits.forEach(elem=>elem.addEventListener('click',show))
+let display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
+
+digits.forEach(elem=>elem.addEventListener('click',show));
 
 function show(){
     if (display.textContent!=0){
@@ -34,3 +37,19 @@ function show(){
     display.textContent = this.textContent;
 }
 
+operators.forEach(elem=>elem.addEventListener('click',operatorPressed));
+
+function operatorPressed(){
+    firstNumber = +display.textContent;
+    display.textContent = '';
+    operator = this.textContent;
+    return operator
+}
+
+equal.addEventListener('click',result);
+
+function result(){
+    secondNumber = +display.textContent;
+    let answer = operate(firstNumber,secondNumber,operator);
+    display.textContent = answer;
+}
