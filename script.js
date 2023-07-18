c3f2e1b444fb95b4382185e2704cda6fc34ba851
Serene The Pity <weekndsonn@gmail.com>
2023-07-18 09:32:59 +0200

    Add reset functionality for Clear

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function multiply(num1,num2){
     return num1 * num2;
 }
 function divide(num1,num2){
+    if (num1 != 0 && num2 === 0) return Error('we dont do infinities');
+    if (num1 ===0 && num2 === 0) return 0;
     return num1 / num2;
 }
 
@@ -21,17 +23,18 @@ function operate(num1,num2,operator){
     if (operator==='/') return divide(num1,num2);
 }
 
-let firstNumber,secondNumber,operator;
+let firstNumber,secondNumber,operator,answer;
 
 let display = document.querySelector('.display');
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear')
 
 digits.forEach(elem=>elem.addEventListener('click',show));
 
 function show(){
-    if (display.textContent!=0){
+    if (display.textContent!=0 && display.textContent != answer){
         return display.textContent += this.textContent;
     }
     display.textContent = this.textContent;
@@ -50,6 +53,17 @@ equal.addEventListener('click',result);
 
 function result(){
     secondNumber = +display.textContent;
-    let answer = operate(firstNumber,secondNumber,operator);
+    answer = operate(firstNumber,secondNumber,operator);
     display.textContent = answer;
+    firstNumber = 0;
+    secondNumber =0;
+}
+
+clear.addEventListener('click',reset);
+
+function reset(){
+    display.textContent = '0';
+    firstNumber = 0;
+    secondNumber = 0;
+    answer = 0;
 }
